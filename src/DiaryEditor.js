8e38b8d3a66bda413ad42d68b1c44f290fefad54
Jeong Hoon <gbg9609@naver.com>
2024-02-23 23:17:05 +0900

    2/23 simplediary

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const DiaryEditor = () => {
-  return <div>오늘의 일기</div>;
+  return (
+    <div>
+      <h2>오늘의 일기</h2>
+      <div>
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryEditor;
