23760437b20ade2f826ff166276a73516cdaf74b
Jeong Hoon <gbg9609@naver.com>
2024-02-24 23:17:48 +0900

    2/24 simple diary

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DiaryEditor = () => {
+  const [author, setAuthor] = useState();
+
   return (
     <div>
       <h2>오늘의 일기</h2>
